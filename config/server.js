d69e1b1e0d7decde6d3df0b7a5cde629e6d5b274
Alonso Barrios <jbarrios@aigroup.com.co>
2022-07-20 18:29:22 -0500

    Primer commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1e277c5bfa4fc877040cc2530a9f53af'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7116e457-3e19-4ae4-998b-d0a3bd0edd82'
+};
